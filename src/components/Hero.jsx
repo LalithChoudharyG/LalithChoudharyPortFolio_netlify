5c7b5ff228fa1cedcc3df92b4cfcc19ec82efa72
Lalith Choudhary <77040979+LalithChoudharyG@users.noreply.github.com>
2025-02-11 16:07:16 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {motion} from 'framer-motion';
 import {styles} from '../styles'
-//import { ComputersCanvas } from './canvas';
+import { ComputersCanvas } from './canvas';
 
 
 const Hero = () => {
@@ -27,7 +27,7 @@ const Hero = () => {
         </div>
       </div>
       
-      {/* <ComputersCanvas/>
+     <ComputersCanvas/>
       <div className="absolute xs:bottom-10 bottom-32 w-full flex
       justify-center items-center">
         <a href="#about">
@@ -47,7 +47,7 @@ const Hero = () => {
             />
           </div>
         </a>
-      </div> */}
+      </div> 
     </section>
   )
 }
