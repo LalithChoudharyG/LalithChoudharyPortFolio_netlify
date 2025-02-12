7e0be6d10bd46f24705306784515d2ca18c1e2fe
Lalith Choudhary <77040979+LalithChoudharyG@users.noreply.github.com>
2025-02-12 00:09:39 -0500

    Update Ball.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,49 +1,49 @@
-import React, {Suspense} from 'react'
-import { Canvas } from '@react-three/fiber'
-import {
-  Decal, Float, OrbitControls, Preload, useTexture
-} from '@react-three/drei'
-import CanvasLoader from '../Loader';
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Decal, Float, OrbitControls, Preload, useTexture } from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
-const Ball = ( props) => {
-  const [decal] =useTexture([props.imgUrl]);
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
+
   return (
-    <Float speed={1.75} rotationIntensity={0.75}
-    floatIntensity={1.4}>
-      <ambientLight intensity={0.7}/>
+    <Float speed={1.75} rotationIntensity={0.75} floatIntensity={1.4}>
+      <ambientLight intensity={0.7} />
       <directionalLight position={[0, 0, 0.05]} intensity={1.5} />
       <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1,1]} /> 
-        <meshStandardMaterial
-          color="#fdfdfe"
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-          /> 
-          <Decal
-            position={[0,0,1]}
-            rotation={[2* Math.PI,0,6.25]}
-            flatShading
-            map={decal}
-          />
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial color="#fdfdfe" polygonOffset polygonOffsetFactor={-5} flatShading />
+        <Decal position={[0, 0, 1]} rotation={[2 * Math.PI, 0, 6.25]} flatShading map={decal} />
       </mesh>
-
     </Float>
-  )
-}
-const BallCanvas=({ icon}) => {
-  return(
+  );
+};
+
+const BallCanvas = ({ icon }) => {
+  const [pixelRatio, setPixelRatio] = useState(window.devicePixelRatio || 1);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setPixelRatio(window.devicePixelRatio || 1);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
     <Canvas
       frameLoop="demand"
-      gl={{preserveDrawingBuffer: true }}
+      dpr={Math.min(pixelRatio, 2)} // Capped at 2 for performance
+      gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false}/>
-        <Ball imgUrl={icon}/>     
+        <OrbitControls enableZoom={false} enableRotate={true} />
+        <Ball imgUrl={icon} />
       </Suspense>
       <Preload all />
     </Canvas>
-  )
-}
+  );
+};
 
-export default BallCanvas
+export default BallCanvas;
